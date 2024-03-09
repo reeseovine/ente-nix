8bdcd087176c2b71ab203cea51132111215276fe
reese sapphire <git@ovine.xyz>
2024-03-09 12:17:51 -0600

    added commented-out nixos module

--- server/flake.nix
+++ server/flake.nix
@@ -34,5 +34,58 @@
 			devShells.default = pkgs.mkShell {
 				buildInputs = with pkgs; [ gcc git go gopls gotools go-tools libsodium musl pkg-config ];
 			};
-		});
+		}); # // {
+		# 	nixosModules.museum = { config, lib, pkgs, ... }:
+		# 		with lib;
+		# 		let
+		# 			cfg = config.services.museum;
+		# 			pkg = self.packages.${pkgs.system}.default;
+		# 		in {
+		# 			options.services.museum = with types; {
+		# 				enable = mkEnableOption (lib.mdDoc "Enable ente.io Museum server");
+
+		# 				package = mkOption {
+		# 					type = types.package;
+		# 					default = pkgs.museum;
+		# 					defaultText = literalExpression "pkgs.museum";
+		# 					description = lib.mdDoc ''
+		# 						The museum package to use.
+		# 					'';
+		# 				};
+
+		# 				dbname = mkOption {
+		# 					type = types.str;
+		# 					default = "museum";
+		# 					description = lib.mdDoc ''
+		# 						Name of postgresql database
+		# 					'';
+		# 				};
+
+		# 				dbuser = mkOption {
+		# 					type = types.str;
+		# 					default = "museum";
+		# 					description = lib.mdDoc ''
+		# 						User for postgresql database
+		# 					'';
+		# 				};
+		# 			};
+
+		# 			config = lib.mkIf cfg.enable {
+		# 				systemd.services.museum = {
+		# 					wantedBy = [ "multi-user.target" ];
+
+		# 					serviceConfig = {
+		# 						Restart = "on-failure";
+		# 						ExecStart = ''
+		# 							${pkgs.museum}/bin/museum
+		# 						'';
+		# 						DynamicUser = true;
+
+		# 						StateDirectory = "museum";
+		# 						WorkingDirectory = "/var/lib/museum";
+		# 					};
+		# 				};
+		# 			};
+		# 		};
+		# };
 }
