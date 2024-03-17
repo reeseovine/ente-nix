9281a8bc4250c11708edf2927aec2184bd4d5f88
reese sapphire <git@ovine.xyz>
2024-03-17 14:40:00 -0500

    flake: rename package to museum; remove devShell and boilerplate module

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 				let
 					pkgs = nixpkgsFor.${system};
 				in {
-				default = pkgs.buildGoModule {
+				museum = pkgs.buildGoModule {
 					pname = "museum";
 					inherit version;
 					src = ./server;
@@ -36,68 +36,5 @@
 			defaultPackage = forAllSystems (system: self.packages.${system}.museum);
 
 			# apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
-
-			# Dependencies that are only needed for development
-			devShells = forAllSystems (system:
-				let
-					pkgs = nixpkgsFor.${system};
-				in {
-					default = pkgs.mkShell {
-						buildInputs = with pkgs; [ gcc git go gopls gotools go-tools libsodium musl pkg-config ];
-					};
-				});
-		}; # // {
-		# 	nixosModules.museum = { config, lib, pkgs, ... }:
-		# 		with lib;
-		# 		let
-		# 			cfg = config.services.museum;
-		# 			pkg = self.packages.${pkgs.system}.default;
-		# 		in {
-		# 			options.services.museum = with types; {
-		# 				enable = mkEnableOption (lib.mdDoc "Enable ente.io Museum server");
-
-		# 				package = mkOption {
-		# 					type = types.package;
-		# 					default = pkgs.museum;
-		# 					defaultText = literalExpression "pkgs.museum";
-		# 					description = lib.mdDoc ''
-		# 						The museum package to use.
-		# 					'';
-		# 				};
-
-		# 				dbname = mkOption {
-		# 					type = types.str;
-		# 					default = "museum";
-		# 					description = lib.mdDoc ''
-		# 						Name of postgresql database
-		# 					'';
-		# 				};
-
-		# 				dbuser = mkOption {
-		# 					type = types.str;
-		# 					default = "museum";
-		# 					description = lib.mdDoc ''
-		# 						User for postgresql database
-		# 					'';
-		# 				};
-		# 			};
-
-		# 			config = lib.mkIf cfg.enable {
-		# 				systemd.services.museum = {
-		# 					wantedBy = [ "multi-user.target" ];
-
-		# 					serviceConfig = {
-		# 						Restart = "on-failure";
-		# 						ExecStart = ''
-		# 							${pkgs.museum}/bin/museum
-		# 						'';
-		# 						DynamicUser = true;
-
-		# 						StateDirectory = "museum";
-		# 						WorkingDirectory = "/var/lib/museum";
-		# 					};
-		# 				};
-		# 			};
-		# 		};
-		# };
+		};
 }
